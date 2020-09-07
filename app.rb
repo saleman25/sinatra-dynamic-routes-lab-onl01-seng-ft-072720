fca01bdf8f16ec055a4649e47e8743fbaced9a2e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-07 23:49:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+get '/reversename/:name' do
+end 
+
+get '/square/:number' do 
+end 
+
+get '/say/:word1/:word2/:word3/:word4/:word5'
+end 
+
+get '/:operation/:number1/:number2'
+end 
 
 end
