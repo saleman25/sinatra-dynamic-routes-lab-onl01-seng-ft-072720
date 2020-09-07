4256e9b7118d3252e741d197df052be0cf487d6b
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-07 23:54:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,10 +19,25 @@ end
 end 
 
 get '/say/:word1/:word2/:word3/:word4/:word5'
-"#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
 end 
 
 get '/:operation/:number1/:number2'
+  number1 = params[:number1].to_i
+  number2 = params[:number2].to_i
+
+  answer = 'Unable to perform this operation'
+
+  case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+end
 end 
 
 end 
