76e22f8b7bf3371feaedc4e6af6a17f99168f59b
Samantha Aleman <samantha.aleman25@gmail.com>
2020-09-07 23:53:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,9 @@ end
   answer
 end 
 
+get '/say/:word1/:word2/:word3/:word4/:word5'
 "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+end 
 
 get '/:operation/:number1/:number2'
 end 
